00cf4ef737410d5e9293f8590019029e4a603a32
Fahri Hanif <68683052+fahrihanif@users.noreply.github.com>
2023-09-12 12:06:35 +0700

    Update Program.cs

--- BasicProgramming/Program.cs
+++ BasicProgramming/Program.cs
@@ -38,6 +38,8 @@ class Program
     static string MyAge(int age)
     {
         switch (age) {
+            case 18:
+                return "Umur 18";
             case 19:
                 return "Umur 19";
             case 20:
